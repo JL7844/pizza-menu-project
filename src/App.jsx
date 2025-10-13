2fd55b5817734cbf1e2b97477f66135567efea9a
Jl7844 <jossylegesse17@gmail.com>
2025-10-13 01:38:27 -0900

    intializing git

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Pizaa from './pizza.jsx'
+const App = () => {
+  return (
+<div>
+  <Pizaa />
+</div>
+  )
+}
+
+export default App
---
+++ src/pizza.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../public/data.js'
+const Pizaa = () => {
+  return (
+    <div>
+        <img src="pizzas/spinaci.jpg" alt="spinaci" />
+        <h2>Pizaa Spinaci</h2>
+        <p>tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  )
+}
+
+export default Pizaa
